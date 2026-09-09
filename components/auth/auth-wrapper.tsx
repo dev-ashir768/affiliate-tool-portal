baf348b9f339578e936390802d6863f53574f315
dev-ashir768 <techashir167@gmail.com>
2026-09-10 01:18:07 +0500

    feat: integrate GSAP animations in AuthWrapper component and update dependencies

--- components/auth/auth-wrapper.tsx
+++ components/auth/auth-wrapper.tsx
@@ -1,16 +1,36 @@
 "use client";
 
 import Image from "next/image";
-import { ReactNode } from "react";
+import { ReactNode, useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 type Props = { children: ReactNode };
 
 export default function AuthWrapper({ children }: Props) {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const imageRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(
+    () => {
+      gsap.from(imageRef.current, {
+        y: "100%",
+        opacity: 0,
+        duration: 1.2,
+        ease: "power3.out",
+        delay: 0.2,
+      });
+    },
+    { scope: containerRef },
+  );
   return (
     <>
       <div className="h-dvh lg:grid lg:grid-cols-2 bg-[#282423]">
-        <div className="hidden w-full h-full lg:flex justify-center items-end">
-          <div>
+        <div
+          ref={containerRef}
+          className="hidden w-full h-full lg:flex justify-center items-end"
+        >
+          <div ref={imageRef} className="will-change-transform">
             <Image
               src="/images/mockups/mobile-in-hand.png"
               alt="mobile-in-hand"
