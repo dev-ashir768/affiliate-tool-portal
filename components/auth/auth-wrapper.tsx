770402f11746d81ed1d9279a9202c81bf11db01e
dev-ashir768 <techashir167@gmail.com>
2026-09-10 00:44:06 +0500

    style: improve image quality setting in AuthWrapper component for better visual presentation

--- components/auth/auth-wrapper.tsx
+++ components/auth/auth-wrapper.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import { ReactNode } from "react";
 
-
 type Props = { children: ReactNode };
 
 export default function AuthWrapper({ children }: Props) {
@@ -12,7 +11,14 @@ export default function AuthWrapper({ children }: Props) {
       <div className="h-dvh lg:grid lg:grid-cols-2 bg-[#282423]">
         <div className="hidden w-full h-full lg:flex justify-center items-end">
           <div>
-            <Image src="/images/banners/mobile-in-hand.png" alt="mobile-in-hand" width={800} height={500} className="lg:w-3xl" />
+            <Image
+              src="/images/banners/mobile-in-hand.png"
+              alt="mobile-in-hand"
+              width={800}
+              height={500}
+              quality={75}
+              className="lg:w-3xl"
+            />
           </div>
         </div>
         <div className="w-full h-full flex flex-col justify-center items-center lg:rounded-l-8xl overflow-hidden bg-white">
