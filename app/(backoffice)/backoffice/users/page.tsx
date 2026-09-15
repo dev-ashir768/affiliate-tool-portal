dca29cbd5081c394aa1edb045b4e3d46a9fee694
dev-ashir768 <techashir167@gmail.com>
2026-09-16 01:10:38 +0500

    feat: add loading state to UsersTable with Suspense
    - Wrapped UsersTable in a Suspense component to display a loading skeleton while data is being fetched.
    - Integrated DataTableSkeleton as a fallback UI during loading.

--- app/(backoffice)/backoffice/users/page.tsx
+++ app/(backoffice)/backoffice/users/page.tsx
@@ -1,5 +1,11 @@
+import { Suspense } from "react";
 import { UsersTable } from "@/components/backoffice/users/users-table";
+import { DataTableSkeleton } from "@/components/data-table/data-table-skeleton";
 
 export default function UsersPage() {
-  return <UsersTable />;
+  return (
+    <Suspense fallback={<DataTableSkeleton />}>
+      <UsersTable />
+    </Suspense>
+  );
 }
